38dd5be8a1168d71502437dafcf7b30f1103b5aa
vadimsky <vribak@gmail.com>
2017-07-31 08:32:32 +0300

    creation service

---
+++ src/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const componentName = 'App service (your service name)';
+
+async function start(logger, config) {
+    const logger = plogger.child({ component: componentName, fn: 'start' });
+    logger.info('Starting');
+
+    const stopHttpController = await startHttpController(plogger, config, rabbitmq);
+
+    return async function stop(force = false) {
+        logger.info({ fn: 'stop', force }, 'Stopping');
+        await stopHttpController();
+        logger.info({ fn: 'stop', force }, 'Stopped');
+    };
+}
+
+module.exports = { start };
---
+++ src/service.js
@@ -0,0 +1,78 @@
+'use strict';
+
+
+
+
+
+async function start () {
+
+    let removeProcessEventListeners;
+    let app;
+
+    const start = async () => {
+        removeProcessEventListeners = appendProcessEventListeners(serviceLogger, stop);
+
+        try {
+
+            // Init additional services (mongo client, redis client, ets)
+            app = await Controller.start(serviceLogger, config);
+        }
+        catch (err) {
+            logger.error({ err }, err.message);
+            stop({ reason: err.reason });
+            throw err;
+        }
+
+        logger.info('Service started');
+    };
+
+
+    const stop = async ({ reason = 'UserRequested' } = {}) => {
+        const logger = serviceLogger.child({ fn: 'stop', reason, stopReason, isStopped, isStopping });
+        if (isStopping || isStopped) {
+            logger.warn(`'stop()' called but service is already stopping or stopped`);
+            return;
+        }
+
+        logger.info('Stopping service');
+        isStopping = true;
+        stopReason = reason;
+        removeProcessEventListeners();
+
+        if (stopController) {
+            await stopApp();
+        }
+
+        isStopped = true;
+        logger.info('Service stopped');
+    };
+
+}
+
+function appendProcessEventListeners(logger, stopService) {
+    const logger = logger.child({ fn: 'appendProcessEventListeners' });
+
+    const handlers = {
+        warning(err) {
+            logger.warn({ err }, 'Process warning');
+            throw err;
+        },
+        unhandledRejection(err, promise) {
+            logger.error({ err, promise }, 'Unhandled rejection');
+            throw err;
+        },
+        uncaughtException(err) {
+            logger.fatal({ err }, 'Uncaught exception');
+            stopService({ reason: 'UncaughtException' });
+        },
+        SIGTERM() {
+            logger.info('Process received SIGTERM');
+            stopService({ reason: 'SIGTERM' });
+        }
+    };
+
+    each(handlers, (handler, name) => process.addListener(name, handler));
+
+    return () => each(handlers, (handler, name) => process.removeListener(name, handler));
+}
+module.export = {start};
